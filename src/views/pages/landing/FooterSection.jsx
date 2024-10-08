6b39c27dd2cfc79cd8e98d128f52d615eaf361ea
marcoguevararodriguez <marco2029guerod@outlook.com>
2024-10-08 14:46:23 -0400

    landing page adjusted according to tim-105

--- src/views/pages/landing/FooterSection.jsx
+++ src/views/pages/landing/FooterSection.jsx
@@ -1,19 +1,15 @@
 // material-ui
 import { useTheme, styled } from '@mui/material/styles';
-import { Box, Container, Grid, IconButton, Link, Stack, Typography } from '@mui/material'; // Divider
+import { Container, Grid, Link, Stack, Typography } from '@mui/material'; // Divider
+import { Instagram, Facebook, Twitter, LinkedIn } from '@mui/icons-material';
+import { red } from '@mui/material/colors';
 
 // project import
-import Chip from 'ui-component/extended/Chip';
-import { frameworks } from './FrameworkSection';
 
 // assets
 // import Dribble from 'assets/images/landing/footer-dribble.png';
 // import Freepik from 'assets/images/landing/footer-freepik.png';
 // import Awards from 'assets/images/landing/footer-awards.png';
-
-import PublicIcon from '@mui/icons-material/Public';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
 
 // Link - custom style
 const FooterLink = styled(Link)(({ theme }) => ({
@@ -29,6 +25,7 @@ const FooterLink = styled(Link)(({ theme }) => ({
 // =============================|| LANDING - FOOTER SECTION ||============================= //
 
 const FooterSection = () => {
+    // eslint-disable-next-line no-unused-vars
     const theme = useTheme();
     const textColor = 'white';
 
@@ -42,24 +39,12 @@ const FooterSection = () => {
         <>
             <Container>
                 <Grid container spacing={6}>
-                    {/* <Grid item xs={12}>
-                        <Stack spacing={4.25}>
-                           
-                            <Stack
-                                direction="row"
-                                justifyContent="center"
-                                spacing={{ xs: 1.5, sm: 6, md: 10, lg: 12 }}
-                                sx={{ overflow: 'hidden' }}
-                            >
-                                <img src={Dribble} alt="dribble" />
-                                <img src={Freepik} alt="freepik" />
-                                <img src={Awards} alt="awards" />
-                            </Stack>
-                            <Divider sx={dividerSX} />
-                        </Stack>
-                    </Grid> */}
                     <Grid item xs={12}>
-                        <Grid container spacing={8}>
+                        <Grid
+                            container
+                            spacing={4}
+                            sx={{ borderBottom: '1px solid white', borderTop: '1px solid white', paddingBottom: 2, paddingTop: 1 }}
+                        >
                             <Grid item xs={12} md={4}>
                                 <Stack spacing={{ xs: 2, md: 5 }}>
                                     <Typography variant="h4" color={textColor} sx={{ fontWeight: 500 }}>
@@ -79,22 +64,15 @@ const FooterSection = () => {
                                                 Explore us
                                             </Typography>
                                             <Stack spacing={{ xs: 1.5, md: 2.5 }}>
-                                                <FooterLink href="#" target="_blank" underline="none">
+                                                <FooterLink href="about-us" target="_blank" underline="none">
                                                     About us
                                                 </FooterLink>
-                                                <FooterLink href="https://codedthemes.gitbook.io/berry/" target="_blank" underline="none">
+                                                <FooterLink href="/contact-us" target="_blank" underline="none">
                                                     Contact us
                                                 </FooterLink>
-                                                <FooterLink
-                                                    href="https://codedthemes.gitbook.io/berry/changelog"
-                                                    target="_blank"
-                                                    underline="none"
-                                                >
+                                                <FooterLink href="/blog" target="_blank" underline="none">
                                                     Blog
                                                 </FooterLink>
-                                                {/* <FooterLink href="https://codedthemes.support-hub.io/" target="_blank" underline="none">
-                                                    Support
-                                                </FooterLink> */}
                                             </Stack>
                                         </Stack>
                                     </Grid>
@@ -134,28 +112,29 @@ const FooterSection = () => {
                                             </Stack>
                                         </Stack>
                                     </Grid>
-                                    {/* <Grid item xs={6} sm={3}>
-                                        <Stack spacing={{ xs: 3, md: 5 }}>
-                                            <Typography variant="h4" color={textColor} sx={{ fontWeight: 500 }}>
-                                                Free Versions
-                                            </Typography>
-                                            <Stack spacing={{ xs: 1.5, md: 2.5 }}>
-                                                <FooterLink href="https://links.codedthemes.com/Yfkxg" target="_blank" underline="none">
-                                                    Free React MUI
-                                                </FooterLink>
-                                                <FooterLink href="https://links.codedthemes.com/epTmN" target="_blank" underline="none">
-                                                    Free Bootstrap 5
-                                                </FooterLink>
-                                                <FooterLink href="https://links.codedthemes.com/seQKN" target="_blank" underline="none">
-                                                    Free Angular
-                                                </FooterLink>
-                                                <FooterLink href="https://links.codedthemes.com/Wfbiy" target="_blank" underline="none">
-                                                    Free Django
-                                                </FooterLink>
-                                            </Stack>
-                                        </Stack>
-                                    </Grid> */}
                                 </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Grid container spacing={8}>
+                            <Grid item xs={12}>
+                                <Stack
+                                    direction={{ xs: 'column', sm: 'column' }} // Changed to column for stacking
+                                    alignItems="center"
+                                    justifyContent="center"
+                                    spacing={{ xs: 1.5, sm: 1, md: 3 }}
+                                >
+                                    <Typography color="white" align="center" sx={{ paddingTop: 2 }}>
+                                        All Rights Reserved ©timekeeper.com
+                                    </Typography>
+                                    <Stack direction="row" alignItems="center" spacing={{ xs: 3, sm: 1.5, md: 2 }}>
+                                        <Instagram sx={{ color: 'white', '&:hover': { color: 'white' } }} />
+                                        <Facebook sx={{ color: 'white', '&:hover': { color: 'white' } }} />
+                                        <Twitter sx={{ color: 'white', '&:hover': { color: 'white' } }} />
+                                        <LinkedIn sx={{ color: 'white', '&:hover': { color: 'white' } }} />
+                                    </Stack>
+                                </Stack>
                             </Grid>
                         </Grid>
                     </Grid>
